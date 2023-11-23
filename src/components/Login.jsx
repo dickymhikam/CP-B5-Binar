1597cf58e91d4ac60691af7f2158b79f6abfeae4
AwangDarmawan <adarmawan468@gmail.com>
2023-11-23 23:14:44 +0700

    perubahan di awal

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,52 +5,34 @@ import React from "react";
 const Login = () => {
   return (
     <>
-      <div id="main-wrapper" className="container pt-5">
-        <div className="row justify-content-center">
-            <div className="col-xl-10">
-                <div className="card border-0">
-                    <div className="card-body p-0 ">
-                        <div className="row">
-                            <div className="col-lg-7">
-                                <div className="p-5">
-                                        <h3 className="txtmasuk font-bold">Masuk</h3>
-                                    <form>
-                                        <div className="control-form mb-3">
-                                            <label htmlFor="email">Email / No Telepon</label>
-                                          <input type="email" 
-                                          className="form-control"  
-                                          id="email"
-                                          placeholder="Contoh:Johndoe@gmail.com"/>
-                                        </div>
-                          
-                                        <div className="control-form mb-3">
-                                          <label htmlFor="password">Password</label> 
-                                          <input 
-                                          type="password" 
-                                          className="form-control"  
-                                          id="password"
-                                          placeholder="Masukan Password"/>
-                                        </div>
-                          
-                                        <div className="d-grid mb-5 ">
-                                          <button className="btn btn-masuk" type="submit">Masuk</button>
-                                        </div>
-
-                                        <p className="control-form text-muted text-center">Belum punya akun? <a href="/register" className="text-decoration-none"> Daftar di sini</a></p>
-                                      </form>
-                                </div>
-                            </div>
-                            <div className="col-lg-5 d-none d-sm-inline-block">
-                                <div className="account-block rounded-right">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        
-    </div>
+      <div className="row auth-coba ">
+      <div className=" col-lg-7 my-auto d-flex justify-content-center">
+          <div className="px-5">
+          <h3 className="txtmasuk font-bold">Masuk</h3>
+          <form className='fm'>
+              <div className="control-form mb-3  ">
+              <label>Email / No Telepon</label>
+                <input type="email" 
+                className="form-control"  
+                placeholder="Contoh:Johndoe@gmail.com"/>
+              </div>      
+              <div className="control-form mb-3">
+                <label>Password</label> 
+                <input 
+                type="password" 
+                className="form-control"  
+                placeholder="Masukan Password"/>
+              </div>
+              <div className="d-grid mb-5 ">
+                <button className="btn btn-masuk" type="submit">Masuk</button>
+              </div>
+              <p className="control-form text-muted text-center">Belum punya akun? <a href="/register" className="text-decoration-none a"> Daftar di sini</a></p>
+            </form>
+          </div>
+      </div>
+      <div className="col-lg-5 account-block">
+      </div>
+</div>
     </>
   );
 };
--- src/components/register.jsx
+++ src/components/register.jsx
@@ -3,72 +3,50 @@ import React from 'react'
 
 const register = () => {
   return (
-      <div id="main-wrapper" className="container">
-        <div className="row justify-content-center">
-            <div className="col-xl-10">
-                <div className="card border-0">
-                    <div className="card-body p-0">
-                        <div className="row">
-                            <div className="col-lg-7">
-                                <div className="p-5">
-                                        <h3 className="txtmasuk font-bold ">Daftar</h3>
-                                    <form>
-                                        <div className="control-form mb-3 ">
-                                        <label>Nama</label>
-                                          <input 
-                                           type="text"
-                                           className="form-control"
-                                            placeholder="Nama Lengkap" required/>
-                                        </div>
-                          
-                                        <div className=" control-form mb-3">
-                                            <label>Email</label>
-                                          <input type="email" 
-                                          className="form-control"  
-                                          placeholder="Contoh:Johndoe@gmail.com"/>
-                                        </div>
-                          
-                                        <div className="control-form mb-3">
-                                            <label>Nomor Telepon</label>
-                                          <input 
-                                          type="text" 
-                                          className="form-control" 
-                                           placeholder="+62"/>
-                                        </div>
-                          
-                                        <div className="control-form mb-3">
-                                          <label>Buat Password</label>
-                                          <input 
-                                          type="password" 
-                                          className="form-control"  
-                                          placeholder="Masukan Password"/>
-                                        </div>
-                          
-                                        <div className="d-grid mb-5 ">
-                                          <button className="btn btn-masuk" type="submit">Daftar</button>
-                                        </div>
-                          
-                                        <p className="control-form text-muted text-center mt-3 mb-0 ">Sudah punya akun? <a href="/login" className="text-decoration-none">Masuk di sini</a></p>
-                                      </form>
-                                </div>
-                            </div>
+    <div className="row auth-coba ">
+    <div className=" col-lg-7 my-auto d-flex justify-content-center">
+        <div className="px-5">
+        <h3 className="txtmasuk font-bold">Daftar</h3>
+        <form>
+          <div className="control-form mb-3 ">
+            <label>Nama</label>
+              <input 
+               type="text"
+               className="form-control"
+                placeholder="Nama Lengkap"/>
+            </div> 
+            <div className="control-form mb-3  ">
+            <label>Email</label>
+              <input type="email" 
+              className="form-control"  
+              placeholder="Contoh:Johndoe@gmail.com"/>
+            </div>  
 
-                            
-    
-                            <div className="col-lg-5 d-none d-lg-inline-block">
-                                <div className="account-block rounded-right">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                  
-                </div>
-               
-    
+            <div className="control-form mb-3">
+              <label>Nomor Telepon</label>
+            <input 
+            type="text" 
+            className="form-control" 
+             placeholder="+62"/>
+          </div>
+
+            <div className="control-form mb-3">
+              <label>Buat Password</label> 
+              <input 
+              type="password" 
+              className="form-control"  
+              placeholder="Masukan Password"/>
             </div>
+            <div className="d-grid mb-5 ">
+              <button className="btn btn-masuk" type="submit">Masuk</button>
+            </div>
+            <p className="control-form text-muted text-center">Belum punya akun? <a href="/Login" className="text-decoration-none a "> Daftar di sini</a></p>
+          </form>
         </div>
-        
     </div>
+    <div className="col-lg-5 account-block">
+    </div>
+</div>
   )
 }
 
