451b35926f78a00dfd7c727b63a505f6e21d680a
Vil <v.andhira16@gmail.com>
2023-11-27 21:33:35 +0700

    update otp script

--- src/components/Auth/CardOTP.jsx
+++ src/components/Auth/CardOTP.jsx
@@ -1,33 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const CardOTP = () => {
     const [lastInputStatus, setLastInputStatus] = useState(0);
-    const inputs = Array.from({ length: 6 }, () => useState(''));
+    const [otpInputs, setOtpInputs] = useState(Array.from({ length: 6 }, () => ''));
+    const [isSimpanEnabled, setIsSimpanEnabled] = useState(false);
 
     const handleKeyUp = (e, index) => {
         const currentElement = e.target;
         const nextIndex = index + 1;
         const prevIndex = index - 1;
-    
+
         if (prevIndex >= 0 && e.keyCode === 8) {
-          if (lastInputStatus === 1) {
-            inputs[prevIndex][1]('');
-            document.getElementById(`otp-input-${prevIndex}`).focus();
-          }
-          setLastInputStatus(1);
-        } else {
-          const reg = /^[0-9]+$/;
-          if (!reg.test(currentElement.value)) {
-            currentElement.value = currentElement.value.replace(/D/g, '');
-          } else if (currentElement.value) {
-            if (nextIndex < inputs.length) {
-              document.getElementById(`otp-input-${nextIndex}`).focus();
-            } else {
-              setLastInputStatus(0);
+            if (lastInputStatus === 1) {
+                const newInputs = [...otpInputs];
+                newInputs[prevIndex] = '';
+                setOtpInputs(newInputs);
+                document.getElementById(`otp-input-${prevIndex}`).focus();
             }
-          }
+            setLastInputStatus(1);
+        } else {
+            const reg = /^[0-9]+$/;
+            if (!reg.test(currentElement.value)) {
+                currentElement.value = currentElement.value.replace(/D/g, '');
+            } else if (currentElement.value) {
+                const newInputs = [...otpInputs];
+                newInputs[index] = currentElement.value;
+                setOtpInputs(newInputs);
+
+                if (nextIndex < otpInputs.length) {
+                    document.getElementById(`otp-input-${nextIndex}`).focus();
+                } else {
+                    setLastInputStatus(0);
+                }
+            }
         }
     };
+
+    useEffect(() => {
+        const allInputsFilled = otpInputs.every(input => input !== '');
+        setIsSimpanEnabled(allInputsFilled);
+    }, [otpInputs]);
 
     return (
         <>
@@ -35,39 +47,24 @@ const CardOTP = () => {
                 <h1>Masukkan OTP</h1>
                 <p className="text-center">Ketik 6 digit kode yang dikirimkan ke J*****@gmail.com</p>
                 <div className="otp-card-inputs">
-                    {inputs.map((input, index) => (
+                    {otpInputs.map((input, index) => (
                         <input
-                        key={index}
-                        id={`otp-input-${index}`}
-                        type="text"
-                        maxLength="1"
-                        value={input[0]}
-                        onChange={(e) => {
-                            input[1](e.target.value);
-                            handleKeyUp(e, index);
-                        }}
+                            key={index}
+                            id={`otp-input-${index}`}
+                            type="text"
+                            maxLength="1"
+                            value={input}
+                            onChange={(e) => {
+                                handleKeyUp(e, index);
+                            }}
                         />
                     ))}
                 </div>
                 <p className="text-center">
                     Kirim Ulang OTP dalam 60 detik <a href="#">Kirim Ulang</a>
                 </p>
-                <button disabled>Simpan</button>
+                <button disabled={!isSimpanEnabled}>Simpan</button>
             </div>
-            {/* <div className="otp-card">
-                <h1>Masukkan OTP</h1>
-                <p className="text-center">Ketik 6 digit kode yang dikirimkan ke J*****@gmail.com</p>
-                <div className="otp-card-inputs">
-                    <input type="text" maxLength="1" autoFocus/>
-                    <input type="text" maxLength="1"/>
-                    <input type="text" maxLength="1"/>
-                    <input type="text" maxLength="1"/>
-                    <input type="text" maxLength="1"/>
-                    <input type="text" maxLength="1"/>
-                </div>
-                <p className="text-center">Kirim Ulang OTP dalam 60 detik <a href="#">Kirim Ulang</a></p>
-                <button disabled>Simpan</button>
-            </div> */}
         </>
     )
 }
